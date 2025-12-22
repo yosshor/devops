9eb7db605c7c38aff6c4b3a58ce8c1c90ef639f4
ShorY <yosishor28@gmail.com>
2025-12-22 13:16:54 +0200

    Update .gitignore to exclude Terraform key files from version control

---
+++ terraform/exercise2/InstId.tf
@@ -0,0 +1,22 @@
+data "aws_ami" "amiID" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"]
+
+}
+
+
+output "instance_id" {
+  description = "AMI ID of Ubunto instance"
+  value       = data.aws_ami.amiID.id
+}
