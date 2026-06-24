3f3cc906e398c26616645fdd828fbd267e7ed9f1
ShorY <yosishor28@gmail.com>
2026-06-24 17:39:17 +0300

    Add support for Amazon Linux in web.sh with yum package management

--- vpc/vpcAndResources/web.sh
+++ vpc/vpcAndResources/web.sh
@@ -7,3 +7,16 @@ wget https://www.tooplate.com/zip-templates/2117_infinite_loop.zip
 unzip -o 2117_infinite_loop.zip
 cp -r 2117_infinite_loop/* /var/www/html/
 systemctl restart apache2
+
+
+#!/bin/bash
+# Amazon Linux uses yum/dnf and the httpd package (not apt/apache2)
+yum update -y
+yum install -y httpd wget unzip
+systemctl start httpd
+systemctl enable httpd
+cd /tmp
+wget https://www.tooplate.com/zip-templates/2117_infinite_loop.zip
+unzip -o 2117_infinite_loop.zip
+cp -r 2117_infinite_loop/* /var/www/html/
+systemctl restart httpd
